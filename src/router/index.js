67de121141084d077ddb02d836fdcf49d3871f4a
muratcankirdar <muratcankirdar@gmail.com>
2022-05-01 21:48:10 +0300

    add Album view to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import AlbumView from '../views/AlbumView.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView,
+  },
+  {
+    path: '/album',
+    name: 'album',
+    component: AlbumView,
   },
 ];
 
