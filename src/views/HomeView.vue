b23bdea11fe04a7f11c1d5064e0b98d1ee8ad6cf
muratcankirdar <muratcankirdar@gmail.com>
2022-05-01 21:45:04 +0300

    implement open album view with next page button

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,28 @@
 <script>
 export default {
   name: 'HomeView',
+  methods: {
+    openAlbum() {
+      this.$router.push('album');
+    },
+  },
 };
 </script>
 
 <template>
   <v-container id="home" class="mt-16">
     <v-row justify="center" align="center" class="my-16">
-      <v-col cols="4" md="2" class="hidden-sm-and-down" />
+      <v-col cols="4" md="2" class="hidden-sm-and-down"/>
 
       <v-col cols="12" sm="12" md="8" class="text-center">
-        <img src="@/assets/trendyol-tech-logo.png" alt="Sticker Set Logo" class="company-logo" />
+        <img src="@/assets/trendyol-tech-logo.png" alt="Sticker Set Logo" class="company-logo"/>
       </v-col>
 
       <v-col
         cols="12" md="2"
         class="text-end animate__animated animate__headShake animate__infinite"
       >
-        <v-btn text depressed class="text-capitalize">
+        <v-btn text depressed class="text-capitalize" @click="openAlbum">
           Next Page
           <v-icon>mdi-arrow-right</v-icon>
         </v-btn>
