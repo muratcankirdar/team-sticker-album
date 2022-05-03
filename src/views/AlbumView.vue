6fad08f6aea57122118cec158d2350f4e9af500c
muratcankirdar <muratcankirdar@gmail.com>
2022-05-03 11:40:44 +0300

    implement team cards in album view

--- src/views/AlbumView.vue
+++ src/views/AlbumView.vue
@@ -1,11 +1,29 @@
 <script>
+import TeamCard from '@/components/TeamCard.vue';
+
 export default {
   name: 'AlbumView',
+  components: { TeamCard },
+  computed: {
+    teams() {
+      return this.$store.getters.teams;
+    },
+  },
 };
 </script>
 
 <template>
   <div>
-    Album View
+    <v-row justify="center" align="center">
+      <v-col
+        v-for="team in teams"
+        :key="team.name"
+        cols="12" md="6" xl="3"
+        align-self="center"
+        class="mx-auto"
+      >
+        <team-card :team="team" />
+      </v-col>
+    </v-row>
   </div>
 </template>
