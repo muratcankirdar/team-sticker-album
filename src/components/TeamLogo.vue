2d912cc80f3adc4b9397c2dc2698986415fb41e7
muratcankirdar <muratcankirdar@gmail.com>
2022-05-04 11:00:30 +0300

    create team logo component

---
+++ src/components/TeamLogo.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'TeamLogo',
+  props: {
+    src: {
+      type: String,
+      default: '',
+    },
+    alt: {
+      type: String,
+      default: 'Team Logo',
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <slot name="image">
+      <img id="team-logo" :src="src" :alt="alt">
+    </slot>
+  </div>
+</template>
+
+<style lang="scss">
+#team-logo {
+  max-width: 128px;
+}
+</style>
