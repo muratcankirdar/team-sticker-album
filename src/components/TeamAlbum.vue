6d21aaa2e1d4ddfa67c8c0893caf5fcfbc937aaf
muratcankirdar <muratcankirdar@gmail.com>
2022-05-04 11:00:43 +0300

    create/init team album component

---
+++ src/components/TeamAlbum.vue
@@ -0,0 +1,30 @@
+<script>
+import source from '@/mixins/source';
+import TeamLogo from '@/components/TeamLogo.vue';
+
+export default {
+  name: 'TeamAlbum',
+  components: { TeamLogo },
+  mixins: [source],
+  props: {
+    team: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <v-row>
+      <v-col cols="12">
+        <team-logo :src="setTeamLogo(team.name)" class="mx-auto" />
+
+        <p>{{ team.name }} - {{ team.slogan }}</p>
+
+        <p>{{ team.description }}</p>
+      </v-col>
+    </v-row>
+  </div>
+</template>
